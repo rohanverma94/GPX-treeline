131d970a2f3a76f1c2080e5b6a0ee72d4285ae09
rohanverma94 <rohan40594@gmail.com>
2023-07-24 02:37:49 +0530

    Added compiler flags and mechanics for safety checks.

--- lib/easy_utils/easy_utils.cpp
+++ lib/easy_utils/easy_utils.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "easy_utils.h"
+/*
+ * Look at https://cwe.mitre.org/data/definitions/467.html
+ * sizeof() on a pointer can produce side effect on malloc()!
+ * sizeof(...)` on the pointer type can result in a dynamic allocation being dumped into the pointer.
+ * This could not be caught because C's malloc() yields void*.
+ * If malloc() had a typed returned, this would have been caught by the type system.
+ */
+#define safe_alloc(...) \
+     (__VA_ARGS__*)malloc(sizeof(__VA_ARGS__))
