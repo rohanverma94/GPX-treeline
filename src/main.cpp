4853af8e84748f653412d1001ea6bf9d5399e8c7
rohanverma94 <rohan40594@gmail.com>
2023-07-24 07:08:41 +0530

    handle db init

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,9 @@ int main() {
 	options.key_hints.page_fill_pct = 50;
 	options.key_hints.num_keys = options.key_hints.records_per_page();
 
-	assert(tl::DB::Open(options, dbPath, &db_).ok());
+	tl::DB::Open(options, dbPath, &db_);
+	assert(db_ != nullptr);
+	//Key Insertion
 	auto key_as_int = "2122";
 	const tl::Slice key(key_as_int);
 	const tl::Slice value = "b21";
@@ -43,14 +45,15 @@ int main() {
 
 
 	//Bulk update!
-	const std::vector<std::pair<const tl::Slice, const tl::Slice>> unsorted ={
+	const std::vector<std::pair<const tl::Slice, const tl::Slice>> unsorted = {
 			{"9",  "ello"},
-			{"7", "hello"},
-			{"11",  "world!"}};
+			{"7",  "hello"},
+			{"11", "world!"}};
 
-	 tl::Status b = db_->BulkLoad({.sorted_load=true},(std::vector<std::pair<const tl::Slice, const tl::Slice>> &) unsorted);
+	tl::Status b = db_->BulkLoad({.sorted_load=true},
+								 (std::vector<std::pair<const tl::Slice, const tl::Slice>> &) unsorted);
 	assert(b.ok());
-	for(auto elem : unsorted) {
+	for (auto elem: unsorted) {
 		db_->Get(tl::ReadOptions(), tl::Slice(elem.first), &val);
 		std::cout << "Value for key=" << elem.first.ToString() << " is " << val << "\n";
 	}
